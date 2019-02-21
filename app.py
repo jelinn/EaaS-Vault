3a4587b0271a96db073e17e55f8589e509689f80
Justin Linn <jelinn@gmail.com>
2019-02-21 16:11:44 -0600

    Decrypt working

--- app.py
+++ app.py
@@ -22,7 +22,20 @@ def viewCustomers():
 @app.route('/getCreds', methods=["GET"])
 def getCredentials():
   client = controller.authDynamo()
-    
+
+@app.route('/decryptId',methods=["GET","POST"])    
+def decryptCustomer():
+  rows = controller.dynamoReadItems()
+    # Call Vault to decrypt customerID, reassign to row, send to template to print
+    #row = controller.decrypt 
+  return render_template("decryptCustomers.html",rows=rows)
+
+@app.route('/viewDecryptId',methods=["GET","POST"])
+def decrypt():
+  if request.form.get("decrypt"):
+    print request.form.get("decrypt")
+    decryptedId=controller.decryptVault(request.form.get("decrypt"))
+    return decryptedId
 
 @app.route('/add', methods=["GET","POST"])
 def addCustomer():
--- controller.py
+++ controller.py
@@ -14,18 +14,41 @@ def writeVault(path,text):
    
 def encryptVault(plainText):
   client = authVault()
-  return client.write('transit/encrypt/my-key',plaintext = base64.b64encode(plainText)
+  print base64.b64encode(plainText)
+  return client.write('transit/encrypt/my-key',plaintext = base64.b64encode(plainText))
 
 def decryptVault(cipherText):
   client = authVault()
-  return writeVault('transit/decrypt/my-key',ciphertext=cipherText) 
+  encodedText = client.write('transit/decrypt/my-key',ciphertext=cipherText) 
+  return base64.b64decode(encodedText["data"]["plaintext"])
+
+def getAwsCredential():
+  client = authVault()
+  return client.read('aws/creds/dynamo-role')
 
 def authVault():
   client = hvac.Client(url=os.environ['VAULT_ADDR'], token=os.environ['VAULT_TOKEN'])
   assert client.is_authenticated()
   return client
 
+def authDynamo():
+  #vault_sesison = authVault()
+  credential = getAwsCredential()
+  print credential
+  AWS_ACCESS_KEY=credential['data']['access_key']
+  AWS_SECRET_KEY=credential['data']['secret_key']
+  print("Access Key : ", AWS_ACCESS_KEY)
+  print("SECRET KEY : ", AWS_SECRET_KEY)
+  print("AWS Cred = ", credential)
+  #dynamo_client = boto3.client('dynamodb',
+  #                            aws_access_key_id=AWS_ACCESS_KEY,
+  #                            aws_secret_access_key=AWS_SECRET_KEY,
+  #                            region_name='us-east-2'
+  #                            )  
+  #return dynamo_client
+
 def dynamoCreateItem(name,id):
+  #dynamo_client = authDynamo()
   response = dynamo_client.put_item(
     TableName='jlinn-demo-CustomerData',
     Item={'customerName':{"S": name}, 'customerId':{"S":id} }
